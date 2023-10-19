4d265bffdbf3028181243dab047f294f5cd8daf0
Luke Boswell <lukewilliamboswell@gmail.com>
2023-10-19 16:04:09 +1100

    scripts build WIP Netlify and dev

--- www/wip_new_website/copy-static.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# We are using this script as a workaround, because using basic-cli Cmd module 
-# doesn't want to copy more than one file at a time
-
-cp -r static/* dist/wip/
